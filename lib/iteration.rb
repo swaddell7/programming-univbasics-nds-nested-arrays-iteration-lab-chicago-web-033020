060540bfd6980801dc6bba8ac9fb86777541fda4
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 19:13:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,8 +12,12 @@ def join_ingredients(src)
   
   while row_index < src.length do 
     new_array << "I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza"
+    
     row_index += 1 
+    
   end 
+  
+  new_array
   
 end
 
