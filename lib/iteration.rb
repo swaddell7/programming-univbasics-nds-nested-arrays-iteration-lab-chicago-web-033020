405b8a818701b8dcf4733fc0257471e9df6a4f17
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 20:23:50 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -60,8 +60,8 @@ def total_even_pairs(src)
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do 
-      if src[row_index][element_index] % 2 == 0 
-        total += src[row_index][element_index]
+      if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+        total += src[row_index][0] + src[row_index][1]
       end
       element_index += 1 
     end 
