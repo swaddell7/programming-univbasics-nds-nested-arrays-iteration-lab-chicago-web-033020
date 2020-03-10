a71c3374a8de7a5c7cfba7853e7a32ad711a8ab3
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 19:39:20 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -29,7 +29,7 @@ def find_greater_pair(src)
   new_array = []
   row_index = 0 
   
-  while row_index < src.length { 
+  while row_index < src.length do  
     
     if src[row_index][0] > src[row_index][1] {
       new_array << src[row_index][0]
