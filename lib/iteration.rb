3acf635ae348fc4b7189abf128b927b3a69d7c4d
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-10 20:02:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -68,18 +68,5 @@ def total_even_pairs(src)
     row_index += 1 
   end 
   total
-        
-    
-  #  if src[row_index][0] % 2 == 0
-  #    total += src[row_index][0]
-  #  elsif src[row_index][1] % 2 == 0 
-  #    total += src [row_index][1]
-  #  end 
-  #  
-  #  row_index += 1 
-  #  
-  #end  
-  #
-  #total
-  
+
 end
